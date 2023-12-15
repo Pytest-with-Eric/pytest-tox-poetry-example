875cbe0ff690289edf344716dbb21adbd5707d53
ericsalesdeandrade <sdaeric19@gmail.com>
2023-12-15 10:09:26 +0530

    add poetry

---
+++ README.md
@@ -0,0 +1,1 @@
+# Build with Poetry and Tox
--- src/calculator.py
+++ src/calculator.py
@@ -16,9 +16,9 @@ def division(a: int | float, b: int | float) -> int | float:
     return a / b
 
 
-if __name__ == "__main__":
-    print(addition(2, 3))
-    print(subtraction(2, 3))
-    print(multiplication(2, 3))
-    print(division(2, 3))
+# if __name__ == "__main__":
+#     print(addition(2, 3))
+#     print(subtraction(2, 3))
+#     print(multiplication(2, 3))
+#     print(division(2, 3))
     # print(division(2, 0))
